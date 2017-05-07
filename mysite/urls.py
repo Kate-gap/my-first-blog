566d614463bf376a891d460fcf0bd2ac7b04ed2b
Your Name <you@example.com>
2017-05-07 10:23:30 +0300

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include (admin.site.urls)),
-    url(r'', include('blog.urls'))
+    url(r'', include('blog.urls')),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
 ]
